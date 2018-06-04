bfba93cfbec8d6beee07ddf39e0b5562cecab58a
LiuSusie <75984637@qq.com>
2018-06-04 22:51:33 +0800

    add resource folder

--- src/main/java/com/selenium/seleniumtest3/chrometest.java
+++ src/main/java/com/selenium/seleniumtest3/chrometest.java
@@ -2,7 +2,7 @@ package com.selenium.seleniumtest3;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
